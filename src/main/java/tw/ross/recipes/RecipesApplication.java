985a2bdff955ec98eb9dbde2a638f995b75ec617
Ross Wilson <ross.wilson@ross.pink>
2022-11-02 17:12:12 +0100

    First commit

---
+++ src/main/java/tw/ross/recipes/RecipesApplication.java
@@ -0,0 +1,9 @@
+package tw.ross.recipes;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/recipes")
+public class RecipesApplication extends Application {
+
+}
